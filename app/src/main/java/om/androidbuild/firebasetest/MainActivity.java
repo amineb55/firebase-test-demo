8160b37683f27f9bb8071bef18e48d88946620a2
amineb55 <49524796+amineb55@users.noreply.github.com>
2026-06-18 15:28:06 +0100

    Fix package app/src/main/java/om/androidbuild/firebasetest/MainActivity.java

---
+++ app/src/main/java/om/androidbuild/firebasetest/MainActivity.java
@@ -0,0 +1,19 @@
+package om.androidbuild.firebasetest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+public class MainActivity extends Activity {
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        TextView tv = new TextView(this);
+        tv.setText("Firebase connecte via AndroidBuild!");
+        setContentView(tv);
+    }
+}
